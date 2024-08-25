c66d67614a83d2de02079d3256b0d317fcea4224
Ninitha Ram <148867057+Ninitharam@users.noreply.github.com>
2024-08-25 20:30:52 +0200

    Added about the project and list of dependencies

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Ninitha_Project_AI II',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'nltk',
+        'scipy',
+        'jupyter',
+    ],
+    description='Decoding the Language of Wine: Innovative Quality Prediction with NLP and Machine Learning',
+    author='Ninitha Ram Mysore Shantha Ram',
+)
